7e22185928f5ff37265d836deb5373ec5182e7fb
Camila Bressan <44245322+camisbressan@users.noreply.github.com>
2020-09-07 20:44:14 -0300

    alterando nome do bucket

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-35scj-335341"
+    bucket = "hackathon-fiap-v2-35scj-335341"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
